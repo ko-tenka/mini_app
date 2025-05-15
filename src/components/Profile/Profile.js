d66ba88de426a1a398b0195d722bdeae23788a4a
ko-tenka <kotelnikova123.123@gmail.com>
2025-05-16 00:53:23 +0300

    release fix name

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,24 +3,29 @@ import { useEffect, useState } from 'react';
 
 function Profile() {
   const [username, setUsername] = useState('');
+  const [isLoading, setIsLoading] = useState(true); // Статус загрузки
 
   useEffect(() => {
     // Проверяем, есть ли объект Telegram WebApp
     if (window.Telegram && window.Telegram.WebApp) {
-      // Получаем информацию о пользователе
+      // Получаем данные о пользователе
       const user = window.Telegram.WebApp.initDataUnsafe;
 
-      // Проверяем, есть ли у пользователя никнейм
       if (user && user.username) {
-        setUsername(user.username);
+        setUsername(user.username);  // Устанавливаем никнейм, если он существует
       }
+
+      // Устанавливаем статус загрузки как завершённый
+      setIsLoading(false);
     }
   }, []);
 
   return (
     <div className="profile">
       <h1>Мой профиль</h1>
-      {username ? (
+      {isLoading ? (
+        <p>Загрузка...</p>
+      ) : username ? (
         <p>Никнейм: {username}</p>
       ) : (
         <p>Привет</p>
